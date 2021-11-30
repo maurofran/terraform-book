6c77f0211dbacb55e17448450cf688b00df9da77
Mauro Franceschini <mauro.franceschini@gmail.com>
2021-11-30 18:17:41 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0a49b025fffbbdac6"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
